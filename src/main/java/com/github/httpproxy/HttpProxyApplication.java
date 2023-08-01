c0fa130dddabea652e6f67eb6cc382357f9e71a6
lxhcaicai <2778763221@qq.com>
2023-08-01 21:51:12 +0800

    git init

---
+++ src/main/java/com/github/httpproxy/HttpProxyApplication.java
@@ -0,0 +1,13 @@
+package com.github.httpproxy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HttpProxyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HttpProxyApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/github/httpproxy/HttpProxyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.httpproxy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HttpProxyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
